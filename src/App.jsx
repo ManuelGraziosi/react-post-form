e8331771fd4c52f76400b65cdfcc39e916e3ea52
ManuelGraziosi <manuel.graziosi@gmail.com>
2025-11-19 23:28:15 +0100

    feat: form submit completed, API connection in progress

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,45 @@ function App() {
     body: "",
     public: false
   }
-
-  function handleChange(propName, propValue) {
-
-  }
-
   const [formData, setFormData] = useState({ initialFormData })
 
+  function handleChange(event) {
+    const newFormData = { ...formData }
+    const changedAtribute = event.target.name;
+    let newValue = event.target.value;
+    if (changedAtribute === "public") {
+      newValue = event.target.checked;
+    }
+    newFormData[changedAtribute] = newValue;
+    setFormData(newFormData);
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(formData)
+  }
   return (
     <>
       <div className="container">
         <h2>Form con POST dei dati:</h2>
-        <form>
+        <form type="submit">
           <div className="mb-3">
             <label htmlFor="author" className="form-label">Nome Autore:</label>
-            <input type="text" value={formData.name} className="form-control" id="author" aria-describedby="author" name="author" />
+            <input type="text" value={formData.name} onChange={handleChange} className="form-control" id="author" aria-describedby="author" name="author" />
           </div>
           <div className="mb-3">
             <label htmlFor="title" className="form-label">Titolo del post:</label>
-            <input type="text" value={formData.title} className="form-control" id="title" aria-describedby="title" name="title" />
+            <input type="text" value={formData.title} onChange={handleChange} className="form-control" id="title" aria-describedby="title" name="title" />
           </div>
           <div className="mb-3">
             <label htmlFor="body" className="form-label">Testo del post:</label>
-            <textarea value={formData.body} className="form-control" id="body" aria-describedby="body" name="body" />
+            <textarea value={formData.body} onChange={handleChange} className="form-control" id="body" aria-describedby="body" name="body" />
           </div>
           <div className="mb-3 form-check">
             <label className="form-check-label" htmlFor="public">rendere pubblico il post?</label>
-            <input type="checkbox" checked={formData.public} className="form-check-input" id="public" name="public" />
+            <input type="checkbox" checked={formData.public} onChange={handleChange} className="form-check-input" id="public" name="public" />
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
         </form>
       </div>
     </>
